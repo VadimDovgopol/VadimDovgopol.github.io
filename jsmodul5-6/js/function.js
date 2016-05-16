2eddbf6360ca8a9ff5a024a53abe468f3b53b07a
VadimDovgopol <electrovadim@gmail.com>
2016-05-17 00:48:08 +0300

    hm 5-6

---
+++ jsmodul5-6/js/function.js
@@ -0,0 +1,66 @@
+function Stopwatch(elem) {
+  var time = 0;
+  var interval;
+  var offset;
+
+  function update() {
+    if (this.isOn) {
+      var timePassed = delta();
+      time += timePassed;
+    }
+
+    var formattedTime = timeFormatter(time);
+    elem.textContent = formattedTime;
+  }
+
+  function delta() {
+    var now = Date.now();
+    var timePassed = now - offset;
+    offset = now;
+    return timePassed;
+  }
+
+  function timeFormatter(timeInMilliseconds) {
+    var time = new Date(timeInMilliseconds);
+    var minutes = time.getMinutes().toString();
+    var seconds = time.getSeconds().toString();
+    var milliseconds = time.getMilliseconds().toString();
+
+    if (minutes.length < 2) {
+      minutes = '0' + minutes;
+    }
+
+    if (seconds.length < 2) {
+      seconds = '0' + seconds;
+    }
+
+    while (milliseconds.length < 3) {
+      milliseconds = '0' + milliseconds;
+    }
+
+    return minutes + ' : ' + seconds + ' . ' + milliseconds;
+  }
+
+  this.isOn = false;
+
+  this.start = function() {
+    if (!this.isOn) {
+      interval = setInterval(update.bind(this), 10);
+      offset = Date.now();
+      this.isOn = true;
+    }
+  };
+
+  this.stop = function() {
+    if (this.isOn) {
+      clearInterval(interval);
+      interval = null;
+      this.isOn = false;
+    }
+  };
+
+  this.reset = function() {
+    time = 0;
+    update();
+  };
+}
--- jsmodul5-6/js/main.js
+++ jsmodul5-6/js/main.js
@@ -4,12 +4,21 @@ var buttonClear = document.getElementById('buttonClear');
 var watch = new Stopwatch(timer);
 
 buttonStart.addEventListener('click', function(){
+
    if (watch.isOn) {
        watch.stop();
+       buttonStart.textContent = 'Start';
+
    } else {
+
        watch.start();
+       buttonStart.textContent = 'Pause';
    }
-});
+})
+
 buttonClear.addEventListener('click', function(){
+
     watch.reset();
-});
+
+})
+
