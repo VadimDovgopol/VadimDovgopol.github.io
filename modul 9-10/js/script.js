617f1847f495dada2371ff24664fc801706a96d7
VadimDovgopol <electrovadim@gmail.com>
2016-06-19 14:51:38 +0300

    hm

--- modul 9-10/js/script.js
+++ modul 9-10/js/script.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
     $( '.dropdown' ).hover(
         function(){
             $(this).children('.submenu').slideDown(200);
+           
         },
         function(){
             $(this).children('.submenu').slideUp(200);
