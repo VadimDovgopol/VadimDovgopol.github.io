101cd81efcfd8cf7b33fb76c20efec9e009d82d8
VadimDovgopol <electrovadim@gmail.com>
2016-06-24 01:25:17 +0300

    hm

--- jsmodul17-18/gruntfile.js
+++ jsmodul17-18/gruntfile.js
@@ -1,17 +1,4 @@
-//// Project configuration.
-//grunt.initConfig({
-//  concat: {
-//    options: {
-//      separator: ';',
-//    },
-//    dist: {
-//      src: ['js/*.js'],
-//      dest: 'minjs/csript.main.js',
-//    },
-//  },
-//    grunt.loadNpmTasks('grunt-contrib-concat');
-//    grunt.loadNpmTasks('grunt-contrib-uglify');
-//});
+
 module.exports = function(grunt) {
 
     grunt.initConfig({
