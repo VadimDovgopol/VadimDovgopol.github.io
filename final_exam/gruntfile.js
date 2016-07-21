df1230ba8dc1e76b3d3a08e799ff83d94c7ff507
VadimDovgopol <electrovadim@gmail.com>
2016-07-21 21:56:56 +0300

    done

--- final_exam/gruntfile.js
+++ final_exam/gruntfile.js
@@ -1,18 +1,19 @@
 module.exports = function (grunt) {
     grunt.initConfig({
-//         concat: {
-//          dist: {
-//            src: ['js/*.js'],
-//            dest: 'minjs/script.main.js',
-//          }
-//        },
-//        uglify: {
-//            js: {
-//                  src: 'minjs/script.main.js'
-//                , dest: 'minjs/script.main.min.js'
-//            }
-//        }
-//        ,
+
+         concat: {
+          dist: {
+            src: ['js/*.js'],
+            dest: 'minjs/script.main.js',
+          }
+        },
+        uglify: {
+            js: {
+                  src: 'minjs/script.main.js'
+                , dest: 'minjs/script.main.min.js'
+            }
+        }
+        ,
         sass: {
             dist: {
                 files: [{
@@ -29,11 +30,35 @@ module.exports = function (grunt) {
                 files: ['styles/*.scss']
                 , tasks: ['sass']
             , }
-        }
+        },
+
+        concat_css: {
+    options: {
+      // Task-specific options go here.
+    },
+    all: {
+      src: ["css/*.css"],
+      dest: "css/styles123.css"
+    }
+  },
+cssmin: {
+  options: {
+    shorthandCompacting: false,
+    roundingPrecision: -1
+  },
+  target: {
+    files: {
+      'output.css': ['css/styles123.css', 'css/reset.css'],
+    }
+  }
+}
+
     });
-//    grunt.loadNpmTasks('grunt-contrib-concat');
-//    grunt.logtadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-concat-css');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.registerTask('default', ['sass']);
+    grunt.registerTask('default', [ 'sass','concat', 'uglify','cssmin','concat_css']);
 };
--- final_exam/minjs/script.main.js
+++ final_exam/minjs/script.main.js
@@ -1,71 +1,39 @@
-//
-//'use strict';
-//
-//$(function(){
-//    function initializeMasonry(){
-//        $container.imagesLoaded( function() {
-//            $container.masonry({
-//                columnWidth: function( containerWidth ) {
-//                    return containerWidth / 3
-//                }
-//            });
-//        });
-//    }
-//    function randomWord (min,max) {
-//        var random = min - 0.5 + Math.random() * (max-min +1);
-//        random = Math.round(random);
-//        return random;
-//    }
-//    function searchPicture(text, quantity) {
-//        if ((text.indexOf('.') !==-1) || (text.indexOf('\\') !==-1) || (text.indexOf('/') !==-1)|| (text.indexOf('!') !==-1) || (text.indexOf('?') !==-1)|| (text.indexOf('-') !==-1)|| (text.indexOf(':') !==-1)) {
-//            $('.grid').append(message);
-//            initializeMasonry();}
-//        else{
-//            $.ajax({
-//                url:'http://pixabay.com/api/?key=2563157-05cd126e344fd2fa56a52a281&q='+text+'&per_page='+quantity,
-//                dataType: 'jsonp'
-//            }).done(function(data) {
-//                if (data.hits.length) {
-//                    var pictureSection = $('#pictureLayout').html();
-//                    var content = _.template(pictureSection)(data);
-//                    $('.grid').append(content);
-//                    initializeMasonry();
-//                }
-//                else {
-//                    $('.grid').append(message);
-//                    initializeMasonry();
-//                }
-//            });
-//        }}
-//    function letsSearch() {
-//        var elem = $('#seek');
-//        string = elem[0].value;
-//        if (string) {
-//            elem[0].value = '';
-//            $container.masonry('destroy');
-//            $('.picture').remove();
-//            searchPicture(string, 12);
-//        }
-//    }
-//    var message ='<figure class="picture item" id="noPic"><img class="radius" src="img/no.jpg" alt="picture"></figure>';
-//    var $container = $('#container');
-//    var pool = ['moto','sport','nature','animals','health','history','relax','cat','flower','auto','computer','robot','science','web','audi','subaru','skoda','bmw'];
-//    var rand;
-//    var string= '';
-//    for (var i=0; i<4; i++) {
-//        rand = randomWord (0,pool.length-1);
-//        searchPicture(pool[rand],3);
-//    }
-//    $('.searching').on('click',function(e){
-//        e.preventDefault();
-//        letsSearch()
-//    });
-//    $('input:text').on('keydown', function(e){
-//        if (e.keyCode == 13){
-//            letsSearch()
-//        }
-//    });
-//});
+function loadGridImages() {
+    var query = $("#textfield").val();
+    var api = "https://pixabay.com/api/";
+    var apiKey = "2714307-54c654f67ca3fd61019f64f4c";
+    var count = 9;
+    $.ajax({
+        method: "GET"
+        , url: api
+        , data: {
+            key: apiKey
+            , per_page: count
+            , q: query
+        }
+        , success: function (data) {
+            var container = $("#gallery");
+            if (data.hits.length) {
+                container.empty();
+                data.hits.forEach(function (imageData) {
+                    var image = buildImage(imageData.webformatURL, imageData.tags);
+                    container.append(image);
+                });
+            }
+        }
+    });
+
+    function buildImage(url, title) {
+        var template = "<div class='item-masonry sizer4'>" + "<img src='{url}' />" + "<div class='idea-content'>" + "<p>{title}</p>" + "</div>" + "</div>";
+        return $(template.replace("{url}", url).replace("{title}", title));
+    }
+    return false;
+};
+$(function () {
+    $("#form").submit(loadGridImages);
+    loadGridImages(); // Load initial random images
+});
+
 (function($) {
     $(function() {
         $('.jcarousel').jcarousel();
