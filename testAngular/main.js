75203e0ea176d23af802b1dbaab123bc92a0530f
VadimDovgopol <electrovadim@gmail.com>
2016-10-10 17:19:06 +0300

    test

---
+++ testAngular/main.js
@@ -0,0 +1,30 @@
+// var app = angular.module('app', []);
+//
+//
+//
+// app.controller('mainCtrl', function () {
+//     this.myLesson = '1lesson';
+//     this.addLesson = function () {
+//         console.log('addLesson');
+//     };
+//
+// });
+//
+// app.controller('firsCtrl', function () {
+//
+//     this.myLesson = '2lesson';
+//
+//
+// });
+//
+// app.controller('secondCtrl', function () {
+// this.myLesson = '3lesson';
+//
+// });
+//
+//
+for (var i = 0; i < 10; i++) {
+    setTimeout(function () {
+        console.log(i);
+    }, 0);
+}
