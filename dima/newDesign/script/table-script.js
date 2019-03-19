8e1cd1e708043c55da9947f6753a47ad29f9d5e9
v.dovgopol <electrovadim@gmail.com>
2019-03-19 12:46:11 +0200

    add new style

--- dima/newDesign/script/table-script.js
+++ dima/newDesign/script/table-script.js
@@ -2,23 +2,13 @@ let app = new Vue({
     el: '#app',
     data: {
         tableContent: [],
-        table: [
-            "LetKnow.news wallet review",
-            "Мобильное приложение",
-            "WEB интерфейс",
-            "Десктоп клиент",
-            "Хранение приватных  ключей у третьей стороны",
-            "Мульти- подпись",
-            "Двухфакторная аутентификация",
-            "User friendly",
-            "Мультивалютный кошелек",
-            "Повышенная анонимность",
-            "Открытый код",
-        ]
+        tableHeader: [],
     },
     mounted() {
         $.getJSON("./table.json", data => {
             this.tableContent = data.item;
+            this.tableHeader = data.table;
         });
-    }
+    },
+    metod
 });
