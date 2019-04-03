24c3cae2064c4a08dbb8264e08d5a6bf7b85c430
v.dovgopol <electrovadim@gmail.com>
2019-04-03 14:42:13 +0300

    add filter

--- dima/newDesign/script/filter.js
+++ dima/newDesign/script/filter.js
@@ -4,12 +4,15 @@ let app = new Vue({
         tableContent: [],
         tableHeader: [],
         filterDataVue: [],
+
         active: "active",
         activeColl: null,
         showDropList: '',
-        filterDataComplete: {},
 
+        filterDataComplete: {},
         allNameArray: [],
+
+
     },
     mounted() {
         this.tableContent = data.item;
@@ -62,22 +65,97 @@ let app = new Vue({
 
         selectAll(value, id) {
 
-            if (this.filterDataVue[id].defaultValue.includes("Все")) {
-                this.filterDataVue[id].all = 'Все';
 
-            } else {
-                this.filterDataVue[id].all = "";
+            if (value === "Все"
+                && this.filterDataVue[id].allTrigger === true) {
+                this.filterDataVue[id].allValue = "";
+                this.filterDataVue[id].defaultValue = [];
+                this.filterDataVue[id].allTrigger = false;
+
+            }
+
+            if (this.filterDataVue[id].defaultValue.includes("Все")
+                && this.filterDataVue[id].allTrigger !== true &&
+                this.filterDataVue[id].allValue !== "Все") {
+
+                this.filterDataVue[id].allTrigger = true;
+                this.filterDataVue[id].allValue = "Все";
+                this.filterDataVue[id].defaultValue = this.filterDataVue[id].checkBox;
+
+            } else if (value !== "Все" && !this.filterDataVue[id].defaultValue.includes("Все")) {
+                this.filterDataVue[id].allValue = "";
+                this.filterDataVue[id].allTrigger = false;
             }
 
 
         },
+        deepEqual(arr) {
+            let cache;
+
+            let ln1 = arr.length;
+            let ln2 = this.filterDataComplete.waysTo.length;
+
+            //теперь делаем цикл
+            for (let i = 0; i < ln1; ++i) {
+                cache = this.filterDataComplete.waysTo[i];
+
+                for (let j = 0; j < ln2; ++j) {
+                    if (cache === arr[j]) {
+                        console.log('найдено совпадение: ' + cache);
+                        return true
+                    }
+                }
+            }
+
+        },
 
         collectData() {
+
             this.filterDataVue.forEach((el) => {
 
                 this.filterDataComplete[el.objName] = el.defaultValue;
 
             });
+
+
+            this.tableContent.forEach((el) => {
+                el.show = true;
+
+
+                if (el.itemFilter.marginTrading !== this.filterDataComplete.marginTrading) {
+                    el.show = false;
+
+                } else if (el.itemFilter.internalToken !== this.filterDataComplete.internalToken) {
+                    el.show = false;
+
+                } else if (el.itemFilter.MotivationOfTraders !== this.filterDataComplete.MotivationOfTraders) {
+                    el.show = false;
+
+                } else if (el.itemFilter.kys !== this.filterDataComplete.kys) {
+                    el.show = false;
+
+                } else if (el.itemFilter.stableCoin !== this.filterDataComplete.stableCoin) {
+                    el.show = false;
+
+                } else if (!this.filterDataComplete.type.includes("Все")
+                    && !this.filterDataComplete.type.includes(el.itemFilter.type)) {
+                    el.show = false;
+
+
+                } else if (!this.filterDataComplete.region.includes("Все")
+                    && !this.filterDataComplete.region.includes(el.itemFilter.region)) {
+                    el.show = false;
+
+                } else if (!this.filterDataComplete.waysTo.includes("Все")
+                    && this.deepEqual(el.itemFilter.waysTo) !== true) {
+
+                    el.show = false;
+
+                } else {
+                }
+            });
+
+
         },
 
         resetFilter() {
